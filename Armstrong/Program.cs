9489e52dd63bb106c20895b2fcd03b72365a8727
rahulramnathbhor <112869809+rahulramnathbhor@users.noreply.github.com>
2022-09-22 13:26:22 +0530

    Add project files.

---
+++ Armstrong/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r = 0, sum = 0, t;
+            Console.WriteLine("Enter Number ");
+            n = int.Parse(Console.ReadLine());
+            t = n;
+            while(n!=0)
+            {
+                r = n % 10;
+                sum = sum + r * r * r;
+                n= n / 10;
+            }
+            if (sum == t)
+                Console.WriteLine("given number is armstrong");
+            else
+                Console.WriteLine("given number is not armstrong");
+        }
+
+    }
+}
